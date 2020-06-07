0cab628c6920f9d910bd04bdf60d91b742ef034b
choushachou <1261066181@qq.com>
2020-06-07 21:20:51 +0800

    第一个Java程序

---
+++ src/zidongtest/test1.java
@@ -0,0 +1,14 @@
+package zidongtest;
+
+/**
+ * Created by Administrator on 2020/6/7.
+ */
+public class test1 {
+    public static void main(String[] args){
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=5;j++){
+                System.out.println(j + "张海洋最好看了");
+            }
+        }
+    }
+}
